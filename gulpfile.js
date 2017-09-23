8911aed66167dafd379ba5e35e11ffc53cd575a6
ivanscm <ivanscm@gmail.com>
2017-09-24 01:27:38 +0700

    app skeleton for materializeCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 var install = require("gulp-install");
 
-var sourceJS = ['./node_modules/vue/dist/vue.min.js', './node_modules/vuetify/dist/vuetify.min.js'];
+var sourceJS = [
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/nette-forms/src/assets/netteForms.min.js',
+    './node_modules/nette.ajax.js/nette.ajax.js',
+    './node_modules/materialize-css/dist/js/materialize.min.js'
+];
 var destinationJS = './www/js';
 
-var sourceCSS = ['node_modules/vuetify/dist/vuetify.min.css'];
-var destinationCSS = 'www/css';
+var sourceCSS = ['./node_modules/materialize-css/dist/css/materialize.min.css'];
+var destinationCSS = './www/css';
+
+var sourceFonts = ['node_modules/materialize-css/dist/fonts/**'];
+var destinationFonts = './www/fonts';
 
 gulp.task('install', function () {
     return gulp
@@ -17,10 +25,14 @@ gulp.task('js', function () {
     return gulp.src(sourceJS)
         .pipe(gulp.dest(destinationJS));
 });
+gulp.task('fonts', function () {
+    return gulp.src(sourceFonts)
+        .pipe(gulp.dest(destinationFonts));
+});
 
 gulp.task('css', function () {
     return gulp.src(sourceCSS)
         .pipe(gulp.dest(destinationCSS));
 });
 
-gulp.task('default', ['install', 'css', 'js']);
+gulp.task('default', ['install', 'css', 'js', 'fonts']);
